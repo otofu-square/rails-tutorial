ab9097d282524a225db6427d2dd7928cbbf9efe0
otofu-square <yukita@feedforce.jp>
2017-03-20 00:10:42 +0900

    :recycle: Use helper method in test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = 'Ruby on Rails Tutorial Sample App'
-  end
-
   test "should get home" do
     get root_url
-    assert_select 'title', @base_title
+    assert_select 'title', full_title
     assert_response :success
   end
 
   test "should get help" do
     get help_url
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', full_title('Help')
     assert_response :success
   end
 
   test "should get about" do
     get about_url
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', full_title('About')
     assert_response :success
   end
 
   test "should get contact" do
     get contact_url
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select 'title', full_title('Contact')
     assert_response :success
   end
 end
