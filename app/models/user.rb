48c8199edaa6fe5e9bb3f7e68fe14bc9480ccf13
otofu-square <yukita@feedforce.jp>
2017-03-20 02:36:03 +0900

    :+1: Add uniqueness validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@
 
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true,
+  validates :email,
+    presence: true,
     length: { maximum: 255 },
-    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
+    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,6 +25,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test 'name should not be too long' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
   test 'email should be present' do
     @user.email = ' '
     assert_not @user.valid?
@@ -59,13 +64,15 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test 'name should not be too long' do
-    @user.name = 'a' * 51
-    assert_not @user.valid?
-  end
-
   test 'email should not be too long' do
     @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
+  end
+
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
