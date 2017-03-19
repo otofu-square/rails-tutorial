3f9043ea15f672b7dc0ea0f0cd4f52eaff6c6d60
otofu-square <yukita@feedforce.jp>
2017-03-20 02:26:46 +0900

    :+1: Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,7 @@
 
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true,
+    length: { maximum: 255 },
+    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 end
