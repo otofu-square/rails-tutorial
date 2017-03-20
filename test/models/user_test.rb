8bfa74a897509e7d417728edae7971db2150ce9b
otofu-square <yukita@feedforce.jp>
2017-03-20 10:21:28 +0900

    :+1: Add valdiation for password_digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,7 +18,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'test', email: 'test@test.com')
+    @user = User.new(
+      name: 'test',
+      email: 'test@test.com',
+      password: 'foobar',
+      password_confirmation: 'foobar'
+    )
   end
 
   test 'should be valid' do
@@ -86,5 +91,17 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = ' ' * 6
+    @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test 'name should have a minimum length' do
+    @user.password = 'a' * 5
+    @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 end
